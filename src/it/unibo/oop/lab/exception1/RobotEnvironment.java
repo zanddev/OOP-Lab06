a152a15f2dee318340c68b2160a7d84485ec20ae
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-28 20:46:45 +0100

    exceptions1: now RobotEnvironment.move throws a PositionOutOfBoundsException

--- src/it/unibo/oop/lab/exception1/RobotEnvironment.java
+++ src/it/unibo/oop/lab/exception1/RobotEnvironment.java
@@ -43,19 +43,19 @@ public class RobotEnvironment {
      *            new position on X
      * @param newY
      *            new position on Y
-     * @return A boolean indicating if the robot moved or not (a robot can move
-     *         only inside the environment's boundaries)
+     * @throws a PositionOutOfBoundException exception if the robot cannot move
+     *            (a robot can move only inside the environment's boundaries)
      */
-    public boolean move(final int newX, final int newY) {
+    public void move(final int newX, final int newY) throws PositionOutOfBoundsException {
         if (newX >= RobotEnvironment.WORLD_X_LOWER_LIMIT
                 && newX <= RobotEnvironment.WORLD_X_UPPER_LIMIT
                 && newY >= RobotEnvironment.WORLD_Y_LOWER_LIMIT
                 && newY <= RobotEnvironment.WORLD_Y_UPPER_LIMIT) {
             this.position.setX(newX);
             this.position.setY(newY);
-            return true;
+            return;
         } else {
-            return false;
+            throw new PositionOutOfBoundsException(newX, newY);
         }
     }
 
